fc1171c114ef484f0d2e45e90777eb200d1cf9ce
Amr-Fekry-Bosila <amrfekryy@gmail.com>
2019-12-13 19:03:31 +0200

    Present one level of data and tigger recursion on item click

--- src/components/RecursiveListComponent.js
+++ src/components/RecursiveListComponent.js
@@ -1,86 +1,62 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
-import { get, map, filter, cloneDeep } from 'lodash'
+import { get, map, filter, cloneDeep, isUndefined } from 'lodash'
 
 class DataList extends Component {
+  constructor (props) {
+    super(props)
+    this.state = {}
+  }
+
+  toggleIDinState = (elementID) => {
+    // console.log(elementID)
+    this.setState({[elementID]: !this.state[elementID]})
+
+    // equivalent to:
+    // if ( isUndefined(this.state[elementID]) ) {
+    //   this.setState({[elementID]: true})
+    //   console.log('added key to state')
+    // } else {
+    //   this.setState({[elementID]: !this.state[elementID]})
+    //   console.log('toggled key in state')
+    // }
+  }
+
   render () {
-    const { state, plan, parentID } = this.props
-    let children = get(state, plan.path, {})
+    // console.log('Component State: ' , this.state)
+    const { reduxState, plan, parentID } = this.props
+    
+    // get list items from redux according to plan.path
+    let children = get(reduxState, plan.path, {})
+    // filter list items if a parentID is passed in recursion
     if (parentID) {
       children = filter(children, { [plan.filterKey]: parentID })
     }
-    // console.log('Plan: ', plan)
-    // console.log('State: ', state)
-    // console.log('parentID: ', parentID)
-    // console.log('Children: ', children)
+
     return (
       <ul>
-        {map(children, child => (
-          <>
-            <li key={child.id}>{child.title}</li>
-            {plan.then &&
-              <DataList state={state} plan={plan.then} parentID={child.id} />}
-          </>
-        ))}
+        { // iterate over list items
+          map(children, child =>
+            <>
+              {/* show item (id is added to state and set to true/false when item is toggled) */}
+              <a onClick={() => this.toggleIDinState(child.id)}>
+                <li key={child.id}>{child.title}</li>
+              </a>
+
+              {/* recurse if (item is in users/tasks level) and (item is toggled to true) */}
+              { plan.then && 
+                this.state[child.id] && 
+                <DataList reduxState={reduxState} plan={plan.then} parentID={child.id} />}
+            </>
+          )
+        }
       </ul>
     )
   }
 }
 
 const mapStateToProps = state => ({
-  state: cloneDeep(state)
+  reduxState: cloneDeep(state)
 })
 
 export default connect(mapStateToProps)(DataList)
-
-// class MapPresentation extends Component {
-//   render () {
-//     return (
-//       <>
-//         <ul>
-//           { 
-//             map(this.props.users, (userData) => {
-//               return (
-//                 <>
-
-//                   <li key={userData.id}> {userData.title}</li>
-//                   <ul>
-//                     {
-//                       map(this.props.tasks, (taskData) => {
-//                         if (taskData.userID === userData.id) {
-//                           const subTasksCountPlan = {
-//                             key: 'filtering',
-//                             path: 'subTasksReducer.data',
-//                             params: { taskID: taskData.id },
-//                             then: { key: 'counting' }
-//                           }
-//                           return (
-//                             <>
-
-//                               <li key={taskData.id}> {taskData.title} <strong>{apply(subTasksCountPlan)}</strong></li>
-//                               <ul>
-//                                 {
-//                                   map(this.props.subTasks, (subTaskData) => {
-//                                     if (subTaskData.taskID === taskData.id) {
-//                                       return <li key={subTaskData.id}>{subTaskData.title}</li>
-//                                     }
-//                                   })
-//                                 }
-//                               </ul>
-
-//                             </>
-//                           )
-//                         }
-//                       })
-//                     }
-//                   </ul>
-
-//                 </>
-//               )
-//             })
-//           }
-//         </ul>
-//       </>
-//     )
-//   }
-// }
